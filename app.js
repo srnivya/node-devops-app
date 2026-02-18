23ccfd7ef03fcebe217d4bfdaf45e08154b7b337
srnivya <nivyasr29@gmail.com>
2026-02-18 17:53:52 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req,res)=>{
+  res.send("Hello from DevOps Capstone Project 🚀");
+});
+
+app.listen(PORT, ()=> console.log(`App running on port ${PORT}`));
